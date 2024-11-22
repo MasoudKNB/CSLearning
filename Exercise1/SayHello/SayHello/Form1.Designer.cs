351da1b73cb32d80a231c1f41627e7428d3c33b2
masoudknb <masoudknb@gmail.com>
2024-11-22 19:03:08 +0330

    Program For Say Hello - 24-22-11

--- Exercise1/SayHello/SayHello/Form1.Designer.cs
+++ Exercise1/SayHello/SayHello/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            BtnSayHello = new Button();
+            BtnClear = new Button();
+            BtnExit = new Button();
+            LblEntrName = new Label();
+            TxtBName = new TextBox();
+            lblSayHello = new Label();
+            SuspendLayout();
+            // 
+            // BtnSayHello
+            // 
+            BtnSayHello.Location = new Point(16, 40);
+            BtnSayHello.Name = "BtnSayHello";
+            BtnSayHello.Size = new Size(97, 26);
+            BtnSayHello.TabIndex = 0;
+            BtnSayHello.Text = "&Say Hello";
+            BtnSayHello.UseVisualStyleBackColor = true;
+            BtnSayHello.Click += BtnSayHello_Click;
+            // 
+            // BtnClear
+            // 
+            BtnClear.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnClear.Location = new Point(134, 40);
+            BtnClear.Name = "BtnClear";
+            BtnClear.Size = new Size(97, 26);
+            BtnClear.TabIndex = 0;
+            BtnClear.Text = "&Clear";
+            BtnClear.UseVisualStyleBackColor = true;
+            BtnClear.Click += BtnClear_Click;
+            // 
+            // BtnExit
+            // 
+            BtnExit.ForeColor = SystemColors.ActiveCaptionText;
+            BtnExit.Location = new Point(252, 40);
+            BtnExit.Name = "BtnExit";
+            BtnExit.Size = new Size(97, 26);
+            BtnExit.TabIndex = 0;
+            BtnExit.Text = "E&xit";
+            BtnExit.UseVisualStyleBackColor = true;
+            BtnExit.Click += BtnExit_Click;
+            // 
+            // LblEntrName
+            // 
+            LblEntrName.AutoSize = true;
+            LblEntrName.Location = new Point(16, 10);
+            LblEntrName.Name = "LblEntrName";
+            LblEntrName.Size = new Size(125, 17);
+            LblEntrName.TabIndex = 1;
+            LblEntrName.Text = "Enter Your Name:";
+            LblEntrName.Click += label1_Click;
+            LblEntrName.MouseLeave += LblEntrName_MouseLeave;
+            LblEntrName.MouseMove += LblEntrName_MouseMove;
+            // 
+            // TxtBName
+            // 
+            TxtBName.Location = new Point(151, 7);
+            TxtBName.Name = "TxtBName";
+            TxtBName.Size = new Size(198, 25);
+            TxtBName.TabIndex = 2;
+            TxtBName.TextChanged += TxtBName_TextChanged;
+            // 
+            // lblSayHello
+            // 
+            lblSayHello.AutoSize = true;
+            lblSayHello.Location = new Point(16, 79);
+            lblSayHello.Name = "lblSayHello";
+            lblSayHello.Size = new Size(0, 17);
+            lblSayHello.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(374, 113);
+            Controls.Add(TxtBName);
+            Controls.Add(lblSayHello);
+            Controls.Add(LblEntrName);
+            Controls.Add(BtnExit);
+            Controls.Add(BtnClear);
+            Controls.Add(BtnSayHello);
+            Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "MyApp ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BtnSayHello;
+        private Button BtnClear;
+        private Button BtnExit;
+        private Label LblEntrName;
+        private TextBox TxtBName;
+        private Label lblSayHello;
     }
 }
--- Exercise1/SayHello/SayHello/Form1.cs
+++ Exercise1/SayHello/SayHello/Form1.cs
@@ -6,5 +6,52 @@ namespace SayHello
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TxtBName_TextChanged(null, null);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblEntrName_MouseMove(object sender, MouseEventArgs e)
+        {
+            LblEntrName.ForeColor = Color.Red;
+        }
+
+        private void LblEntrName_MouseLeave(object sender, EventArgs e)
+        {
+            LblEntrName.ForeColor = Color.Black;
+        }
+
+        private void BtnSayHello_Click(object sender, EventArgs e)
+        {
+            lblSayHello.Text = "Hello " + TxtBName.Text + "!";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void TxtBName_TextChanged(object sender, EventArgs e)
+        {
+            BtnSayHello.Enabled = Convert.ToBoolean(TxtBName.Text.Length);
+            BtnClear.Enabled = Convert.ToBoolean(TxtBName.Text.Length);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtBName.Text = "";
+            lblSayHello.Text = "";
+        }
+
+        private void BtnExit_MouseHover(object sender, EventArgs e)
+        {
+            BtnExit.BackColor = Color.Red;
+        }
     }
 }
